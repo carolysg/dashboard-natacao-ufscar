f991cf2008b21c08a54ac61afab5565c52a44a6d
GHMessias <ghmessias@estudante.ufscar.br>
2023-11-07 18:40:06 -0300

    bugfix

--- streamlitapp.py
+++ streamlitapp.py
@@ -76,8 +76,14 @@ if ano_atleta == 'Todos':
     df_atleta = df[(df['Nome']==atleta)&(df['Prova']==prova_atleta)&(df['Piscina']==piscina_atleta)]
 else:
     df_atleta = df[(df['Nome']==atleta)&(df['Prova']==prova_atleta)&(df['Piscina']==piscina_atleta)&(df['Data'].dt.year==int(ano_atleta))]
+df_atleta = df_atleta.sort_values(by = 'Data')
 df_display = df_atleta.rename(columns={'Tempo_str': 'Tempo oficial'})
+
 st.dataframe(df_display[['Data', 'Categoria', 'Tempo oficial', 'Campeonato', 'Colocação', 'Local']], hide_index=True)
+
+# selecionar só o menor tempo de uma data duplicada
+
+df_atleta = df_atleta.groupby(['Data', 'Prova']).agg({'Tempo' : 'min', 'Campeonato' : 'first', 'Local' : 'first'}).reset_index()
 
 tempo_min = df_atleta['Tempo'].min()
 tempo_min = math.floor(tempo_min)
@@ -91,7 +97,8 @@ def segundos_para_minutos_segundos_centesimos(segundos):
     return f"{minutos}:{segundos_int:02d}.{centesimos:02d}"
 tempo_min_formatado = segundos_para_minutos_segundos_centesimos(tempo_min)
 tempo_max_formatado = segundos_para_minutos_segundos_centesimos(tempo_max)
-num_ticks = 6
+# num_ticks = 6
+num_ticks = tempo_max - tempo_min + 3
 valores_de_ticks_y = np.linspace(tempo_min-1, tempo_max+1, num_ticks)
 valores_de_ticks_y_formatados = [segundos_para_minutos_segundos_centesimos(val) for val in valores_de_ticks_y]
 
@@ -101,7 +108,9 @@ fig.update_traces(mode="markers+lines", line=dict(color="#DD0000", width=4),
                   hoverlabel=dict(bordercolor="white", font=dict(size=18)))
 fig.update_xaxes(
     title='Data',
-    ticktext=df_atleta['Data'].apply(lambda x: x.strftime('%m/%y')),
+    # ticktext=df_atleta['Data'].apply(lambda x: x.strftime('%m/%y')),
+    ticktext=df_atleta['Data'].apply(lambda x: x.strftime('%b/%y')),
+    # ticktext=df_atleta['Data'],
     tickvals=df_atleta['Data'],
     tickfont = dict(size=18, color='white'), 
     titlefont = dict(size=21, color='white')
@@ -121,3 +130,5 @@ fig.update_layout(
 )
 # fig.add_vline(x=pd.to_datetime('2023-07-01'), line_width=3, line_color="blue") # Início dos treinos com Colombo
 st.plotly_chart(fig)
+
+
